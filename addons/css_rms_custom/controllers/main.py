4cc95dc53fe7cd8f9c2ec2f11f9de22b2610d683
Your Name <you@example.com>
2026-03-18 10:38:47 +0100

    Force assign RMS groups to wuseberger@gmail.com and improve homepage redirect priority

--- addons/css_rms_custom/controllers/main.py
+++ addons/css_rms_custom/controllers/main.py
@@ -2,6 +2,10 @@ from odoo import http
 from odoo.http import request
 
 class RootRedirect(http.Controller):
-    @http.route('/', type='http', auth="none")
-    def index(self, **kw):
+    @http.route('/', type='http', auth="none", website=True, sitemap=False, priority=1)
+    def index_redirect(self, **kw):
+        return request.redirect('/web')
+
+    @http.route('/shop', type='http', auth="none", website=True)
+    def shop_redirect(self, **kw):
         return request.redirect('/web')
